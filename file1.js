36af3525aff6e110757b484916a9a48270cfa7be
Gopika <gopikakaushal@gmail.com>
2024-08-12 11:37:09 +0530

    Add files via upload

---
+++ file1.js
@@ -0,0 +1,51 @@
+// let a=10;
+// document.write(a,"<br>")
+// let b=5;
+// document.write(b,"<br>")
+// let sum=a+b; //15
+// document.write(sum,"<br>")
+// let diff=a-b; //5
+// document.write(diff,"<br>")
+// let mul= a*b; //50
+// document.write(mul,"<br>")
+// let quotient= a/b; //2(divide)
+// document.write(quotient,"<br>")
+// let remainder=a%b; //0
+// document.write(remainder,"<br>")
+// let isequal=(a==b);//false
+// document.write(isequal,"<br>")
+// let isNotequal=(a!=b); //true
+// document.write(isNotequal,"<br>")
+// let isgreater=(a>b);//true
+// document.write(isgreater,"<br>")
+// let logicalAnd=(a>b && b<10); //true
+// document.write(logicalAnd,"<br>")
+
+// let fname="Gopika";
+// document.write("Hello ",fname,"<br>")
+// let age =30;
+// document.write('age ',age,"<br>")
+// let isstudent=true;
+// document.write(`student - ${isstudent? "yes":"no"}<br>`)
+// let emptyvalue= null;
+// document.write(`About us ${emptyvalue || "Write something"}<br>`)
+// let uninitialized;
+// document.write(!uninitialized && "Fill details completely")
+
+//initialization +declaration = defination
+
+
+let age=17;
+if(age>17){
+    document.write("Eligible to vote");
+}
+else{
+    document.write("Not Eligible");
+}
+document.write("<br>")
+
+//or
+
+document.write(age>17?"eligible":"not eligible")
+
+
